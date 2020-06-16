a4dd906295ed82504e13b0b3d58b52eda08fcf59
mahadevi <kolkarmahadevi@gmail.com>
2020-06-16 10:31:38 +0530

    second commit

--- GitProject/src/JavaProgram.java
+++ GitProject/src/JavaProgram.java
@@ -3,6 +3,11 @@ public class JavaProgram {
 
 	public static void main(String[] args) {
 	System.out.println("hello");
+	int a=10;
+	int b=20;
+	int c;
+	c=a+b;
+	System.out.println(c);
 
 	}
 
---
+++ GitProject/src/TestingProjec.java
@@ -0,0 +1,15 @@
+
+public class TestingProjec {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		int a=10;
+		int b=20;
+		int c;
+		c=a+b;
+		System.out.println(c);
+
+
+	}
+
+}
