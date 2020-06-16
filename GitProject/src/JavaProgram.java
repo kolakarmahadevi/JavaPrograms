5b5f2ef4ad4b16a5e383259b350178e541fdfa80
mahadevi <kolkarmahadevi@gmail.com>
2020-06-16 10:42:57 +0530

    third commit

--- GitProject/src/JavaProgram.java
+++ GitProject/src/JavaProgram.java
@@ -3,11 +3,14 @@ public class JavaProgram {
 
 	public static void main(String[] args) {
 	System.out.println("hello");
-	int a=10;
+	int a=60;
 	int b=20;
 	int c;
 	c=a+b;
 	System.out.println(c);
+	
+	c=a-b;
+	System.out.println("the subtraction is "+c);
 
 	}
 
